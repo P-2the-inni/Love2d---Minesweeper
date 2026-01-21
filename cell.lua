d4a47661b70df3127b6e189f254d426f186e2cc2
P_2the_inni <98459374+P-2the-inni@users.noreply.github.com>
2026-01-21 13:30:04 +0000

    Refactor cell class

--- cell.lua
+++ cell.lua
@@ -1,9 +1,17 @@
-local cell = {}
-cell.__index = cell
+-- util --
 
 local getBomb = function()
 	return math.random(6) == 1
 end
+
+local posExists = function(x, y, gridSize)
+	return x > 0 and y > 0 and x < gridSize+1 and y < gridSize+1
+end
+
+-- cell class --
+
+local cell = {}
+cell.__index = cell
 
 local getBombCount = function(cellList)
 	local count = 0
@@ -15,11 +23,11 @@ local getBombCount = function(cellList)
 	return count
 end
 
-function cell:new(pos, image, size)
+function cell:new(pos, initialImage, size)
 	local o = { 
 		position = pos,
 		hidden = true,
-		image = image,
+		image = initialImage,
 		size = size,
 		flag = false,
 		unknown = false,
@@ -32,7 +40,7 @@ function cell:draw()
 	love.graphics.draw(self.image, (self.position[1]-1)*self.size, (self.position[2]-1)*self.size, 0, (self.size/16)/2, (self.size/16)/2)
 end
 
-function cell:updateImage()
+function cell:updateImage(images, grid)
 	if self.flag then
 		self.image = images.flag
 	elseif self.unknown then
@@ -42,24 +50,46 @@ function cell:updateImage()
 	elseif self.bomb then
 		self.image = images.bomb_clicked
 	elseif not self.hidden then
-		self:updateCell()
+		self:reveal(images, grid, #grid)
 	end
 end
 
-function cell:updateCell()
-	local neighbours = getNeighbours(self.position[1], self.position[2])
+function cell:reveal(images, grid)
+	local neighbours = self:getNeighbours(grid, #grid)
 	local count = getBombCount(neighbours)
 	if count == 0 then
 		self.image = images.blank
 		for i, n in ipairs(neighbours) do
 			if n.hidden and not n.flag and not n.unknown then
 				n.hidden = false
-				n:updateImage(images)
+				n:updateImage(images, grid, #grid)
 			end
 		end
 	else
 		self.image = images[tostring(count)]
 	end
+end
+
+local offsets = {
+	{-1,0},
+	{1,0},
+	{0,1},
+	{0,-1},
+	{1,1}, 
+	{-1,1}, 
+	{1,-1}, 
+	{-1,-1},
+}
+
+function cell:getNeighbours(grid)
+	local neighbours = {}
+	for i, offset in ipairs(offsets) do
+		local checkX, checkY = self.position[1] + offset[1], self.position[2] + offset[2]
+		if posExists(checkX, checkY, #grid) then
+			table.insert(neighbours, grid[checkX][checkY])
+		end
+	end
+	return neighbours
 end
 
 return cell
