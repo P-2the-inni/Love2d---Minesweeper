619681f8c94662c4e982e4337f6e8e48a34a8d79
P_2the_inni <98459374+P-2the-inni@users.noreply.github.com>
2026-01-21 13:14:37 +0000

    Refactor cell module for improved structure and clarity

--- cell.lua
+++ cell.lua
@@ -1,25 +1,18 @@
-cell = {}
-cell.mt = {
-	__index = {
-		draw = function(self)
-			love.graphics.draw(self.image, (self.position[1]-1)*self.size, (self.position[2]-1)*self.size, 0, (self.size/16)/2, (self.size/16)/2)
-		end,
-		updateImage = function(self, img)
-			if self.flag then
-				self.image = img.flag
-			elseif self.hidden then
-				self.image = img.hidden
-			elseif self.bomb then
-				self.image = img.bomb_clicked
-			elseif not self.hidden then
-				updateCell(self)
-			end
-		end
-	}
-}
+local cell = {}
+cell.__index = cell
 
-cell.getBomb = function()
+local getBomb = function()
 	return math.random(6) == 1
+end
+
+local getBombCount = function(cellList)
+	local count = 0
+	for i, n in ipairs(cellList) do
+		if n.bomb then
+			count = count + 1
+		end
+	end
+	return count
 end
 
 function cell:new(pos, image, size)
@@ -30,25 +23,43 @@ function cell:new(pos, image, size)
 		size = size,
 		flag = false,
 		unknown = false,
-		bomb = self.getBomb(),
-		color = 1
+		bomb = getBomb(),
 	}
-	return setmetatable(o, self.mt)
+	return setmetatable(o, self)
 end
 
-function updateCell(c)
-	local neighbours = getNeighbours(c.position[1], c.position[2])
+function cell:draw()
+	love.graphics.draw(self.image, (self.position[1]-1)*self.size, (self.position[2]-1)*self.size, 0, (self.size/16)/2, (self.size/16)/2)
+end
+
+function cell:updateImage()
+	if self.flag then
+		self.image = images.flag
+	elseif self.unknown then
+		self.image = images.unknown
+	elseif self.hidden then
+		self.image = images.hidden
+	elseif self.bomb then
+		self.image = images.bomb_clicked
+	elseif not self.hidden then
+		self:updateCell()
+	end
+end
+
+function cell:updateCell()
+	local neighbours = getNeighbours(self.position[1], self.position[2])
 	local count = getBombCount(neighbours)
 	if count == 0 then
-		c.image = images.blank
+		self.image = images.blank
 		for i, n in ipairs(neighbours) do
-			if n.hidden then
+			if n.hidden and not n.flag and not n.unknown then
 				n.hidden = false
 				n:updateImage(images)
 			end
 		end
 	else
-		c.image = images[tostring(count)]
+		self.image = images[tostring(count)]
 	end
 end
 
+return cell
